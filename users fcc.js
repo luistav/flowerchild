f8f55ffe1fcf5b076dbb25a827ff7ce75837fe7b
luistav <luistavares@me.com>
2025-02-16 08:44:01 +1100

    Create users fcc.js

---
+++ users fcc.js
@@ -0,0 +1,58 @@
+[
+    {
+        "name": "Jin",
+        "rate": 30,
+        "defaultHours": 45,
+        "defaultLeave": 0,
+        "defaultPH": 0
+    },
+    {
+        "name": "Emma",
+        "rate": 27,
+        "defaultHours": 45,
+        "defaultLeave": 0,
+        "defaultPH": 0
+    },
+    {
+        "name": "Handerson",
+        "rate": 24,
+        "defaultHours": 35,
+        "defaultLeave": 0,
+        "defaultPH": 0
+    },
+    {
+        "name": "Jimmy",
+        "rate": 25,
+        "defaultHours": 35,
+        "defaultLeave": 0,
+        "defaultPH": 0
+    },
+    {
+        "name": "Irvin",
+        "rate": 24.5,
+        "defaultHours": 30,
+        "defaultLeave": 0,
+        "defaultPH": 0
+    },
+    {
+        "name": "Peter",
+        "rate": 24,
+        "defaultHours": 32,
+        "defaultLeave": 0,
+        "defaultPH": 0
+    },
+    {
+        "name": "Ly",
+        "rate": 24,
+        "defaultHours": 18,
+        "defaultLeave": 0,
+        "defaultPH": 0
+    },
+    {
+        "name": "Other",
+        "rate": 24.5,
+        "defaultHours": 0,
+        "defaultLeave": 0,
+        "defaultPH": 0
+    }
+]
